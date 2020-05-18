066c4b7be956151f87fb960e071692371b36e5e3
Matt Bauman <mbauman@gmail.com>
2020-05-17 23:27:47 -0500

    🐛 un-debug

--- bin/main.jl
+++ bin/main.jl
@@ -3,6 +3,6 @@ using CovidCountyDash
 const df = download_and_preprocess(joinpath(@__DIR__, "..", "data", "pop2019.csv"))
 @info "Got the data"
 
-handler = make_handler(create_app(df), debug = true)
+handler = make_handler(create_app(df))
 @info "Setup and now serving..."
 HTTP.serve(handler, ip"0.0.0.0", parse(Int, length(ARGS) > 0 ? ARGS[1] : "8080"))
