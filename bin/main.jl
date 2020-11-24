b55a415c4f493e8684fa1bacd6a446e492f2eef3
Matt Bauman <mbauman@gmail.com>
2020-11-24 14:00:44 -0500

    🆙 update all dependencies
    key big ones: Dashboards becomes Dash.jl, DataFrames breaking release

--- bin/main.jl
+++ bin/main.jl
@@ -1,8 +1,32 @@
 using CovidCountyDash
 
+if true
+    # Patch up PlotlyBase with https://github.com/sglyon/PlotlyBase.jl/pull/35
+    import PlotlyBase
+    @eval PlotlyBase function GenericTrace(df::DataFrames.AbstractDataFrame; group=nothing, kind="scatter", kwargs...)
+         d = Dict{Symbol,Any}(kwargs)
+         if _has_group(df, group)
+             _traces = map(dfg -> GenericTrace(dfg; kind=kind, name=_group_name(dfg, group), kwargs...), DataFrames.groupby(df, group))
+             return GenericTrace[t for t in _traces]
+         else
+             if (group !== nothing)
+                 @warn "Unknown group $(group), skipping"
+            end
+        end
+        for (k, v) in d
+            if isa(v, Symbol) && hasproperty(df, v)
+                d[k] = df[!, v]
+            elseif isa(v, Function)
+                d[k] = v(df)
+            end
+        end
+        GenericTrace(kind; d...)
+    end
+end
+
 const df = download_and_preprocess(joinpath(@__DIR__, "..", "data", "pop2019.csv"))
 @info "Got the data"
 
-handler = make_handler(create_app(df))
+app = create_app(df)
 @info "Setup and now serving..."
-HTTP.serve(handler, ip"0.0.0.0", parse(Int, length(ARGS) > 0 ? ARGS[1] : "8080"))
+run_server(app, "0.0.0.0", parse(Int, length(ARGS) > 0 ? ARGS[1] : "8080"))
--- src/CovidCountyDash.jl
+++ src/CovidCountyDash.jl
@@ -1,13 +1,15 @@
 module CovidCountyDash
 import HTTP, CSV
-using DataFrames, Dates, PlotlyBase, Dashboards, Sockets, Statistics
+using Dash, DashCoreComponents, DashHtmlComponents
+using DataFrames, Dates, PlotlyBase, Statistics
+using Base: splat
 
-export download_and_preprocess, create_app, make_handler, HTTP, DataFrame, @ip_str
+export download_and_preprocess, create_app, HTTP, DataFrame, run_server
 
 function download_and_preprocess(popfile)
-    d = CSV.read(IOBuffer(String(HTTP.get("https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv").body)), normalizenames=true)
-    pop = CSV.read(popfile)
-    dd = join(d, pop, on=:fips, kind=:left)
+    d = CSV.read(IOBuffer(String(HTTP.get("https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv").body)), DataFrame, normalizenames=true)
+    pop = CSV.read(popfile, DataFrame)
+    dd = leftjoin(d, pop, on=:fips, matchmissing=:equal)
     # # New York City
     # All cases for the five boroughs of New York City (New York, Kings, Queens, Bronx and Richmond counties) are assigned to a single area called New York City.
     dd[(dd.state .== "New York") .& (dd.county .== "New York City"), :pop] .=
@@ -46,15 +48,13 @@ isset(x) = x !== nothing && !isempty(x)
 rolling(f, v, n) = n == 1 ? v : [f(@view v[max(firstindex(v),i-n+1):i]) for i in eachindex(v)]
 function subset(df, states, counties)
     mask = isset(counties) ? (df.county .∈ (counties,)) .& (df.state .∈ (states,)) : df.state .∈ (states,)
-    return by(df[mask, :], :date, cases=:cases=>sum, deaths=:deaths=>sum, pop=:pop=>sum)
+    return combine(groupby(df[mask, :], :date), :cases=>sum, :deaths=>sum, :pop=>sum, renamecols=false)
 end
-function precompute(df, states, counties; alignment = 10, type=:cases, roll=1, popnorm=false)
-    !isset(states) && return DataFrame(days=Int[],values=Int[],diff=Int[],dates=Date[],location=String[])
+function precompute(df, states, counties; type=:cases, roll=1, popnorm=false)
+    !isset(states) && return DataFrame(values=Int[],diff=Int[],dates=Date[],location=String[])
     subdf = subset(df, states, counties)
-    vals = float.(subdf[:, type])
-    dates = subdf[:, :date]
-    idx = findfirst(vals .>= alignment)
-    crossing = idx !== nothing ? dates[idx] : maximum(dates) + Day(1)
+    vals = float.(subdf[!, type])
+    dates = subdf.date
     if popnorm
         vals .*= 100 ./ subdf.pop
     end
@@ -62,7 +62,7 @@ function precompute(df, states, counties; alignment = 10, type=:cases, roll=1, p
         (length(states) <= 2 ? join(states, " + ") : "$(states[1]) + $(length(states)-1) other states") :
         (length(counties) <= 2 ? join(counties, " + ") * ", " * states[] :
             "$(counties[1]), $(states[]) + $(length(counties)-1) other counties")
-    return DataFrame(days=(x->x.value).(dates .- crossing),values=vals, dates = dates, diff = [missing; rolling(mean, diff(vals), roll)], location=loc)
+    return DataFrame(values=vals, dates=dates, diff=[missing; rolling(mean, diff(vals), roll)], location=loc)
 end
 # Given a state, list its counties
 function counties(df, states)
@@ -76,16 +76,17 @@ function counties(df, states)
     end
 end
 # put together the plot given a sequence of alternating state/county pairs
-function plotit(df, value, logy, type, realign, alignment, roll, popnorm, pp...)
-    alignment = something(alignment, 10)
+function plotit(df, value, type, roll, checkopts, pp...)
     roll = something(roll, 1)
-    data = reduce(vcat, [precompute(df, state, county, type=Symbol(type), alignment=alignment, roll=roll, popnorm=popnorm) for (state, county) in Iterators.partition(pp, 2)])
+    logy = checkopts === nothing ? false : "logy" in checkopts
+    popnorm = checkopts === nothing ? false : "popnorm" in checkopts
+    data = reduce(vcat, [precompute(df, state, county, type=Symbol(type), roll=roll, popnorm=popnorm) for (state, county) in Iterators.partition(pp, 2)])
     data.text = Dates.format.(data.dates, "U d")
     layout = Layout(
-        xaxis_title = realign ? "Days since $alignment total $(type)" : "Date",
+        xaxis_title = "Date",
         yaxis_title = value == "values" ? "Total confirmed $type" :
                       roll > 1 ? "Average daily $type (rolling $roll-day mean)" : "Number of daily $type",
-        xaxis = realign && !isempty(data) ? Dict(:range=>[-1, ceil(maximum(data.days)/5)*5]) : Dict(),
+        xaxis = Dict(),
         yaxis_ticksuffix = popnorm ? "%" : "",
         hovermode = "closest",
         title = string(value == "values" ? "Total " : "Daily " , "Confirmed ", uppercasefirst(type)),
@@ -95,7 +96,7 @@ function plotit(df, value, logy, type, realign, alignment, roll, popnorm, pp...)
     )
     isempty(data) && return Plot(data, layout)
     return Plot(data, layout,
-        x = realign ? :days : :dates,
+        x = :dates,
         y = Symbol(value),
         text = :text,
         group = :location,
@@ -106,7 +107,9 @@ end
 
 function create_app(df;max_lines=6)
     states = sort!(unique(df.state))
-    app2 = Dash("🦠 COVID-19 Tracked by US County", external_stylesheets=["https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"]) do
+    app = dash(external_stylesheets=["https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"])
+    app.title = "🦠 COVID-19 Tracked by US County"
+    app.layout =
         html_div(style=(padding="2%",), [
             html_h1("🦠 COVID-19 Tracked by US County", style=(textAlign = "center",)),
             html_div(style=(width="60%",margin="auto", textAlign="center"), [
@@ -114,16 +117,12 @@ function create_app(df;max_lines=6)
                 html_a("data", href="https://github.com/nytimes/covid-19-data"),
                 " from ",
                 html_a("The New York Times", href="https://www.nytimes.com/interactive/2020/us/coronavirus-us-cases.html"),
-                ", based on reports from state and local health agencies; inspired by ",
-                html_a("John Burn-Murdoch's", href="https://twitter.com/jburnmurdoch"),
-                " ",
-                html_a("analyses", href="https://www.ft.com/coronavirus-latest"),
-                " of national-level data",
+                ", based on reports from state and local health agencies",
                 html_p("Loaded data through $(Dates.format(maximum(df.date), "U d"))",
                     style=(height="2rem", lineHeight="2rem",margin="0")),
                 ]),
-            dbc_row([
-                dbc_col(width=8,
+            html_div(className="row", [
+                html_div(className="col-8",
                     html_table(style=(width="100%",),
                         vcat(html_tr([html_th("State",style=(width="40%",)),
                                       html_th("County",style=(width="60%",))]),
@@ -132,63 +131,64 @@ function create_app(df;max_lines=6)
                               for n in 1:max_lines])
                     )
                 ),
-                dbc_col(width=4, [
+                html_div(className="col-4", [
                     html_b("Options"),
-                    dbc_radioitems(id="type", options=[(label="Confirmed positive cases", value="cases"), (label="Confirmed deaths", value="deaths")], value="cases"),
+                    dcc_radioitems(id="type", labelStyle=(display="block",),
+                        options=[
+                            (label="Confirmed positive cases", value="cases"),
+                            (label="Confirmed deaths", value="deaths")],
+                        value="cases"),
                     html_hr(style=(margin=".25em",)),
-                    dbc_radioitems(id="values", options=[(label="Cumulative", value="values"), (label="New daily cases", value="diff")], value="diff"),
+                    dcc_radioitems(id="values", labelStyle=(display="block",),
+                        options=[
+                            (label="Cumulative", value="values"),
+                            (label="New daily cases", value="diff")],
+                        value="diff"),
                     html_div(id="smoothing_selector", style=(visibility="visible", display="block"), [
                         html_span("Rolling", style=(var"padding-left"="1.5em",)),
-                        dcc_input(id="roll", type="number", placeholder="alignment", min=1, max=10, step=1, value=7, style=(margin="0 .5em 0 .5em",)),
+                        dcc_input(id="roll", type="number", min=1, max=14, step=1, value=7, style=(margin="0 .5em 0 .5em",)),
                         html_span("day mean")
                     ]),
                     html_hr(style=(margin=".25em",)),
-                    html_div(html_label((dbc_checkbox(id="realign", checked=false, style=(margin="0 .5em 0 .1em",)), "Realign by initial value"))),
-                    html_div(id="alignment_selector", style=(visibility="visible", display="block"), [
-                        html_span("Align on", style=(var"padding-left"="1.5em",)),
-                        dcc_input(id="alignment", type="number", placeholder="alignment", min=1, max=10000, step=1, value=10, style=(margin="0 .5em 0 .5em",)),
-                        html_span("total "),
-                        html_span("cases", id="cases_or_deaths")
-                        ]),
-                    html_label((dbc_checkbox(id="popnorm", checked=false, style=(margin="0 .5em 0 .1em",)), "Normalize by population")),
-                    html_br(),
-                    html_label((dbc_checkbox(id="logy", checked=false, style=(margin="0 .5em 0 .1em",)), "Use logarithmic y-axis"))
+                    dcc_checklist(id="checkopts", labelStyle=(display="block",),
+                        options=[
+                            (label="Normalize by population", value="popnorm"),
+                            (label="Use logarithmic y-axis", value="logy")
+                        ])
                 ])
             ]),
             html_div(dcc_graph(id = "theplot", figure=Plot()), style = (width="80%", display="block", margin="auto")),
             html_br(),
-            html_a("Code source (Julia + Plotly Dash + Dashboards.jl)", href="https://github.com/mbauman/covid19",
+            html_span([html_a("Code source", href="https://github.com/mbauman/CovidCountyDash.jl"),
+                " (",  html_a("Julia", href="https://julialang.org"),
+                " + ", html_a("Plotly Dash", href="https://plotly.com/dash/"),
+                " + ", html_a("Dash.jl", href="https://juliahub.com/ui/Packages/Dash/oXkBb"),
+                ")"],
                 style=(textAlign = "center", display = "block"))
         ])
-    end
 
     hide_missing_row(s, c) = !isset(s) && !isset(c) ? (display="none",) : (display="table-row",)
     for n in 2:max_lines
-        callback!(hide_missing_row, app2, CallbackId([], [(Symbol(:state,"-",n), :value), (Symbol(:state,"-",n-1), :value)], [(Symbol(:scrow,"-",n), :style)]))
+        callback!(hide_missing_row, app, Output("scrow-$n", "style"), [Input("state-$n", "value"), Input("state-$(n-1)", "value")])
     end
     for n in 1:max_lines
-        callback!(x->counties(df, x), app2, CallbackId([], [(Symbol(:state,"-",n), :value)], [(Symbol(:county,"-",n), :options)]))
-        callback!(x->nothing, app2, CallbackId([], [(Symbol(:state,"-",n), :value)], [(Symbol(:county,"-",n), :value)]))
+        callback!(x->counties(df, x), app, Output("county-$n", "options"), Input("state-$n", "value"))
+        callback!(x->nothing, app, Output("county-$n", "value"), Input("state-$n", "value"))
     end
-    callback!((args...)->plotit(df, args...), app2, CallbackId([], [(:values, :value); (:logy, :checked); (:type, :value); (:realign, :checked); (:alignment, :value); (:roll, :value); (:popnorm, :checked); [(Symbol(t,"-",n), :value) for n in 1:max_lines for t in (:state, :county)]], [(:theplot, :figure)]))
-    callback!(identity, app2, callid"type.value => cases_or_deaths.children")
-    callback!(app2, callid"type.value => values.options") do type
+    callback!((args...)->plotit(df, args...), app, Output("theplot", "figure"),
+        splat(Input).([("values", "value"); ("type", "value"); ("roll", "value"); ("checkopts", "value");
+                [("$t-$n", "value") for n in 1:max_lines for t in (:state, :county)]]))
+    callback!(identity, app, Output("cases_or_deaths","children"), Input("type","value"))
+    callback!(app, Output("values","options"), Input("type","value")) do type
         return [(label="Cumulative", value="values"), (label="New daily $(type)", value="diff")]
     end
-    callback!(app2, callid"realign.checked => alignment_selector.style") do realign
-        if realign
-            return (visibility="visible", display="block")
-        else
-            return (visibility="hidden", display="none")
-        end
-    end
-    callback!(app2, callid"values.value => smoothing_selector.style") do value
+    callback!(app, Output("smoothing_selector","style"), Input("values","value")) do value
         if value == "diff"
             return (visibility="visible", display="block")
         else
             return (visibility="hidden", display="none")
         end
     end
-    return app2
+    return app
 end
 end
