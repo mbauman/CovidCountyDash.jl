56a3b95c2e1a398de35532ca9eca823d9cbdf16e
Matt Bauman <mbauman@gmail.com>
2022-06-02 19:27:36 -0400

    Slight simplification
    this makes it more obvious that we're indeed downloading all three years by putting them all into YEARS, and since we're vcatting down the line anyhow, a comprehension is a simple way to get everything in one shot

--- src/CovidCountyDash.jl
+++ src/CovidCountyDash.jl
@@ -76,17 +76,14 @@ const STATE_GROUPS = OrderedDict{String, Vector{Int}}(
     "west" => [4, 6, 8, 16, 30, 32, 35, 41, 49, 53, 56],
     )
 
-const YEARS = ["2021", "2022"]
+const YEARS = ["2020", "2021", "2022"]
 const URIBASE = "https://raw.githubusercontent.com/nytimes/covid-19-data/master"
 function download_and_preprocess()
     states = CSV.read(IOBuffer(String(HTTP.get(URIBASE * "/us-states.csv").body)), DataFrame, normalizenames=true)
-    counties = CSV.read(IOBuffer(String(HTTP.get(URIBASE * "/us-counties-2020.csv").body)), DataFrame, normalizenames=true)
-    for year in YEARS
-        county_yr = CSV.read(IOBuffer(String(HTTP.get(URIBASE * "/us-counties-" * year * ".csv").body)), DataFrame, normalizenames=true)
-        append!(counties, county_yr)
-    end
     states[!, :county] .= missing
-    d = transform!(vcat(counties, states),
+    county_yrs = [CSV.read(IOBuffer(String(HTTP.get(URIBASE * "/us-counties-" * year * ".csv").body)), DataFrame, normalizenames=true)
+                    for year in YEARS]
+    d = transform!(vcat(states, county_yrs...),
         [:state, :county, :fips]=>ByRow() do state, county, fips
             if ismissing(fips)
                 if state == "New York" && county == "New York City"
